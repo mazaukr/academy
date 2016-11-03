12c015ec5dc154592d3c117930a261743d586c16
Artem Poltarak <artem.poltarak@gmail.com>
2016-11-03 10:02:53 +0200

    fssf

---
+++ arrays/23/23.php
@@ -0,0 +1,16 @@
+
+<?php
+
+if (!isset($_GET['name'])) {
+$_GET['name']='';}
+else $_GET['name'] =$_GET['name'];
+
+
+
+print_r($_GET);
+echo "<br";
+
+
+?>
+
+
---
+++ arrays/23/controller.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!isset($_GET['name'])) {
+    $_GET['name']='';}
+else $_GET['name'] = clear_data($_GET['name']);
+
+if (!isset($_GET['count'])) {
+    $_GET['count']='';}
+else $_GET['count'] = clear_data($_GET['count']);
+
+if (!isset($_GET['price'])) {
+    $_GET['price']='';}
+else $_GET['price'] = clear_data($_GET['price']);
+
+function clear_data ($value){
+    $value = strip_tags($value);
+    return $value;
+}
+
+session_start();
+
+
+
+?>
+
