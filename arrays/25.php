e75f1115b550c8478b46db9596d90c464d536e25
Artem Poltarak <artem.poltarak@gmail.com>
2016-11-05 17:20:12 +0200

    fssf4r

--- arrays/25.php
+++ arrays/25.php
@@ -3,9 +3,32 @@
 
 <?php
 
+$arr = Array();
+$i=1;
+while ($i<=30){
+    $arr[]=rand (1, 500);
+    $i++;
+}
+$max= max($arr);
+$min = min($arr);
 
+echo "<br>";
+print_r($arr);
+echo "<br>";
 
+for ($i=0;$i<30;$i++){
 
+        if ($arr[$i] == $max) {
+            echo "<br> $arr[$i] MAX <br>";
+            $arr[$i] = $min;
+        }
+        elseif ($arr[$i] == $min) {
+            echo "<br> $arr[$i] MIN <br>";
+            $arr[$i] = $max;
+        }
+}
 
+echo "<br><b> max={$max} <br> min={$min} <br> </b>";
 
+print_r($arr);
 ?>
