e76eb11dc136b96842ef5c97cebac2280ac6a34a
Artem Poltarak <artem.poltarak@gmail.com>
2016-11-06 22:35:59 +0200

    fssf4r

--- functions_forms_tasks/test.php
+++ functions_forms_tasks/test.php
@@ -2,12 +2,14 @@
 
 
 
-$str1 = "Утро вечера мудренее наверное";
-$str2 = "Утро вечера наверное мудренее";
+$str1 = "Utro vechera mudrenee";
+$str2 = "Utro mudrenee vechera ";
 
-$result = strrpos("$str1","$str2");
-echo "$result";
-
+$result = strrpos("$str1","Utro");
+echo "<br />";
+$result2 = strstr("$str2","mudrenee");
+echo "$result <br />";
+echo "$result2";
 
 
 
