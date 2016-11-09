38e3df9bf6195d2e11110e2d55d6b12227085211
Artem Poltarak <artem.poltarak@gmail.com>
2016-11-09 21:42:10 +0200

    fssf4r

--- functions_forms_tasks/3/model.php
+++ functions_forms_tasks/3/model.php
@@ -2,8 +2,32 @@
 
 session_start();
 
+//controller
+if (isset($_POST['lenght']))
+    $lenght = $_POST['lenght'];
+else
+    $lenght = 100;
 
 
 
+$file = fopen('test.txt','r');
+$size = filesize('test.txt');
+$text = fread($file,$size);
+fclose($file);
+
+
+
+
+$arrayorig = explode(' ', $text);
+
+foreach ($arrayorig as $key => $value) {
+    if (mb_strlen($value)>$lenght)
+        $arraynew[] = $value;
+            else
+                continue;
+
+}
+
+print_r($arraynew);
 
 ?>
